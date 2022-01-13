4ccdc0e6e7639df2959293c5035d10634d295726
Christopher Biscardi <chris@christopherbiscardi.com>
2022-01-13 10:58:14 -0800

    Upgrade to Clap v3
    * `structopt::StructOpt` is now `clap::StructOpt`
    * `Opt::from_args()` is now `Opt::parse()`
    * include `env` and `derive` features for clap depedency
    * the short flag is now a character and not a string

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use clap::StructOpt;
 use color_eyre::eyre::{eyre, Result, WrapErr};
 use digital_garden::write;
 use directories::UserDirs;
 use std::path::PathBuf;
-use structopt::StructOpt;
-
 /// A CLI for the growing and curation of a digital garden
 ///
 /// Visit https://www.rustadventure.rs/garden for more!
 #[derive(StructOpt, Debug)]
 #[structopt(name = "garden")]
 struct Opt {
-    #[structopt(parse(from_os_str), short = "p", long, env)]
+    #[structopt(
+        parse(from_os_str),
+        short = 'p',
+        long,
+        env
+    )]
     garden_path: Option<PathBuf>,
 
     #[structopt(subcommand)]
@@ -37,7 +41,7 @@ fn get_default_garden_dir() -> Result<PathBuf> {
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     let garden_path = match opt.garden_path {
         Some(pathbuf) => Ok(pathbuf),
         None => get_default_garden_dir().wrap_err("`garden_path` was not supplied"),
